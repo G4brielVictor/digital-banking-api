9f3514d540c70dad17992bfddcf7afc587fb7aeb
Gabriel <g4brielvictorp@gmail.com>
2026-09-09 15:24:13 -0300

    feat(model): implement Transaction entity and TransactionType with formatted statement

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,4 +1,57 @@
 package model;
 
+import model.enums.TransactionType;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
 public class Transaction {
+
+    private final UUID id = UUID.randomUUID();
+    private final TransactionType type;
+    private final Double amount;
+    private final LocalDateTime timeStamp = LocalDateTime.now();
+    private final String description;
+
+    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+    public Transaction(TransactionType type, double amount, String description) {
+        this.type = type;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "----------------------------------\n" +
+               "ID: " + getId() +
+               "\nData/hora: " + FORMATTER.format(getTimeStamp()) +
+               "\nTipo: " + getType() +
+               "\nValor: " + String.format("R$ %.2f", amount) +
+               "\nDescriço: " + description +
+               "\n----------------------------------\n";
+    }
+
+
 }
