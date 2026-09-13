42ad658ea293c341d089ad2f9964eda130ecf016
Gabriel <g4brielvictorp@gmail.com>
2026-09-13 12:12:20 -0300

    feat(transaction): implement custom toString with DateTimeFormatter and currency formating

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -7,7 +7,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public class Transaction {
-
+    /*"A classe Transaction representa um fato histórico e contábil, que uma vez ocorrido, jamais pode ser adulterado. Utilizei o modificador final
+     e suprimi os setters para garantir a imutabilidade do objeto. Isso assegura a integridade e a rastreabilidade dos dados bancários em conformidade com as regras de auditoria."*/
     private final UUID id = UUID.randomUUID();
     private final TransactionType type;
     private final Double amount;
@@ -44,14 +45,12 @@ public class Transaction {
 
     @Override
     public String toString() {
-        return "----------------------------------\n" +
+        return "\n\n---------------------------------------\n" +
                "ID: " + getId() +
                "\nData/hora: " + FORMATTER.format(getTimeStamp()) +
                "\nTipo: " + getType() +
                "\nValor: " + String.format("R$ %.2f", amount) +
                "\nDescriço: " + description +
-               "\n----------------------------------\n";
+               "\n---------------------------------------\n\n";
     }
-
-
 }
